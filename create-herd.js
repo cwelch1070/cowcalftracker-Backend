d20fc0f5feb63fcc712f29a2eb53bab6b17214f0
Caleb Welch <calebwelch233@gmail.com>
2022-09-15 21:23:32 -0500

    First commit
    Add herds function working as well as buttons to edit, remove, and add cattle

---
+++ create-herd.js
@@ -0,0 +1,31 @@
+//Basically creates a new array and reads herd data into the new array
+let herds = getHerds()
+
+//Gets the hash of the location and trims off the first position in which contains a #
+const herdId = location.hash.substring(1)
+//Checks if the herd.id in memory matches the hash
+let herd = herds.find(function(herd) {
+    return herd.id === herdId
+})
+
+//Defines event handlers
+const nameHerd = document.querySelector('#herd-name')
+const herdCount = document.querySelector('#herd-count')
+const addHerdBtn = document.querySelector('#add-herd')
+
+//Captures text from name input
+nameHerd.addEventListener('input', function(e) {
+    herd.herdName = e.target.value
+    saveHerds(herds)
+})
+
+//Captures number from input
+herdCount.addEventListener('input', function(e) {
+    herd.count = e.target.value
+    saveHerds(herds)
+}) 
+
+//Simple button linking back to index page
+addHerdBtn.addEventListener('click', function(e) {
+    location.assign('./index.html')
+})
---
+++ main.js
@@ -0,0 +1,21 @@
+const herds = getHerds()
+
+const newHerd = document.querySelector('#create-herd')
+
+generateDOM(herds)
+clearAllHerds()
+
+newHerd.addEventListener('click', function(e) {
+    const id = uuidv4()
+    herds.push({
+        id: id,
+        herdName: '',
+        count: 0,
+        date: getCurrentDate()
+    })
+  
+    saveHerds(herds)
+    location.assign(`./createHerd.html#${id}`)
+})
+
+
