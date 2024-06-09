a189f33a87df0b651f17969af165d9b1435b8a50
Caleb Welch <calebwelch233@gmail.com>
2024-06-09 15:33:34 -0500

    Update-2 : Added get all records endpoint and upated shcema

--- server/src/routes/records.js
+++ server/src/routes/records.js
@@ -1,17 +1,26 @@
 const express = require('express')
-const Herd = require('../models/herds')
 const Cattle = require('../models/cattle')
 const Records = require('../models/records')
 const router = new express.Router()
 
 router.post('/api/attendance', async (req, res) => {
-    const cattle = getCattleRecords(req.body.record)
-    const records = buildRecordsObjects(cattle)
+    const cattle = await getCattleRecords(req.body.record)
+    const records = await buildRecordsObjects(cattle)
     
     try {
         await Records.insertMany(records, { ordered: true })
     } catch (error) {
         res.status(200).send('Success')
+    }
+})
+
+router.get('/api/attendance/:id', async (req, res) => {
+    try {
+        const cattle = await Records.find({ herdId: req.params.id }) 
+        
+        res.status(200).send(cattle)
+    } catch (e) {
+        res.status(400).send('Error: ', e)
     }
 })
 
@@ -27,10 +36,14 @@ const getCattleRecords = async (reqBody) => {
 }
 
 const buildRecordsObjects = async (cattle) => {
+    const currDate = new Date()
     const records = []
     cattle.forEach(element => {
         records.push(new Records({
+            cowName: element.name,
             tag: element.tag,
+            accountedFor: true,
+            date: currDate.toLocaleDateString(),
             cowId: element._id,
             herdId: element.herd
         }))
